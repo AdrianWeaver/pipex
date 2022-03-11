0159c1980d3b16ad625c85f87b5fcb86291e9e42
aweaver <aweaver@student.42.fr>
2022-03-11 16:54:44 +0100

    adding lexical information for pipes with defines

--- includes/ft_pipex.h
+++ includes/ft_pipex.h
@@ -6,14 +6,17 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 16:52:50 by aweaver           #+#    #+#             */
-/*   Updated: 2022/03/07 17:02:51 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/03/11 15:31:11 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PIPEX_H
 # define FT_PIPEX_H
 
-#include <unistd.h>
-#include <sys/wait.h>
+# include <unistd.h>
+# include <sys/wait.h>
+# include <fcntl.h>
 
+# define READ_END 0
+# define WRITE_END 1
 #endif
